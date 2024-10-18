18992dcffebd92bf5b29827cd32dfe9c57e26d73
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-10-18 19:27:07 +0530

    Basic routes work

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import PlanMeal from './components/PlanMeal';
 
 function App() {
   return (
-    <HomePage />
+    <>
+    <ResponsiveAppBar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/plan-meal" element={<PlanMeal />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
